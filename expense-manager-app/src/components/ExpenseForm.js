5db28da1668a7bf4211fa7e01d9f68c1cf888961
VeeShostak <VeeShostak@users.noreply.github.com>
2017-10-17 13:55:02 -0400

    React-Redux: dispatch add expense. error checking

--- expense-manager-app/src/components/ExpenseForm.js
+++ expense-manager-app/src/components/ExpenseForm.js
@@ -13,7 +13,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   };
 
   onDescriptionChange = (e) => {
@@ -27,22 +28,45 @@ export default class ExpenseForm extends React.Component {
   onAmountChange = (e) => {
     const amount = e.target.value;
 
-    // use regex to only accept valid currency (decimal optional and limited to 2 places)
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   };
   onDateChange = (selectedDate) => {
-    this.setState(() => ({ createdAt: selectedDate }));
+    // cant clear date, only change it
+    if(createdAt) {
+      this.setState(() => ({ createdAt: selectedDate }));
+    }
+    
   };
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
   };
+  onSubmit = (e) => {
+    e.preventDefault(); // prevent full page refresh
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Provide description and amount' }));
+    } else {
+      this.setState(() => ({ error: '' }));
+
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+
+    }
+  };
+
+  // ==============================
   render() {
     return (
       <div>
 
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
