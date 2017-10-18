6e1949131bad0763b5e92754495387947a4d1f2d
VeeShostak <VeeShostak@users.noreply.github.com>
2017-10-18 15:59:39 -0400

    ExpressJS production web server

---
+++ expense-manager-app/server/server.js
@@ -0,0 +1,24 @@
+// import
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+
+// serve up asset from public dir on requerst
+app.use(express.static(publicPath));
+
+// for GET req on server , serve what is on 
+/// public server or same thing everytime
+// @param1: response object info about req
+// @parama2: request object manipulate respone our express 
+//           server makes to who made the http req
+app.get('*', (req, res) => {
+  // process unhandled requests
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+// start up the server on port 3000
+app.listen(3000, () => {
+  console.log('server is up');
+});
