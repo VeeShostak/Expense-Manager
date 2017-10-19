d04218c94d72ee390ce2b24da2fcc67ef06ff546
VeeShostak <VeeShostak@users.noreply.github.com>
2017-10-19 14:27:22 -0400

    Setup DB  for Development & Production, and Test environments

--- expense-manager-app/src/firebase/firebase.js
+++ expense-manager-app/src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase'; // take all exports and toss them on 1 nam
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyBA3YQAu0bSZn9I3q0O5OXuWLQPwaqlIqA",
-  authDomain: "expense-manager-5de24.firebaseapp.com",
-  databaseURL: "https://expense-manager-5de24.firebaseio.com",
-  projectId: "expense-manager-5de24",
-  storageBucket: "expense-manager-5de24.appspot.com",
-  messagingSenderId: "629338267537"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
