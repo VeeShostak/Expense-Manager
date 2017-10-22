7b8fabd314f651ad245cb925e6dd2cc3788f70a5
VeeShostak <VeeShostak@users.noreply.github.com>
2017-10-22 13:04:15 -0400

    styling inputs

--- expense-manager-app/src/components/ExpenseForm.js
+++ expense-manager-app/src/components/ExpenseForm.js
@@ -77,19 +77,20 @@ export default class ExpenseForm extends React.Component {
   render() {
     return (
       <div>
-
         {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
             autoFocus
+            className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
             type="text"
             placeholder="Amount"
+            className="text-input"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
@@ -103,13 +104,13 @@ export default class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            className="textarea"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
           </textarea>
           <button>Add Expense</button>
         </form>
-
       </div>
     )
   }
